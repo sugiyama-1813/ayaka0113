96618952869fa745fd58e7e59ea382abe31e7be3
sugiyama-1813 <zm26090@josai.ac.jp>
2026-06-29 11:47:24 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -4,7 +4,7 @@ alert(Math.sqrt(1+1));
 alert(Math.sqrt(2)+Math.sqrt(3));
 alert(Math.sqrt(1/0));
 alert(Math.sqrt(0/0));
-alert(Math.sqrt(2+3));
+alert(Math.sqrt (2+3));
 alert(Math.sqrt('2'+'3'));
 alert(Math.sqrt(Number('2')+Number('3'));
 alert(Math.sqrt('2'+3));
